722e440cfe477b9b5add7d418447f5e43de95f7e
David Lee <daviddwlee84@gmail.com>
2019-10-29 14:28:00 +0800

    Add additional relation extraction dataset

--- README.md
+++ README.md
@@ -11,9 +11,55 @@ Get raw data:
 ## Dataset
 
 * [AGENDA Dataset](https://github.com/rikdz/GraphWriter#agenda-dataset)
+* [davidsbatista/Annotated-Semantic-Relationships-Datasets](https://github.com/davidsbatista/Annotated-Semantic-Relationships-Datasets)
+  * DBpediaRelations-PT
 
 ### AGENDA Dataset
+
+> all-in-one json
 
 * Train: 38720
 * Valid: 1000
 * Test: 1000
+
+### SemEval 2007 Task 4
+
+> each relation (total 7) a file
+
+* Train (0-140): 140 x 7
+* Test (141-?)
+  * relation 1: 220
+  * relation 2: 218
+  * relation 3: 233
+  * relation 4: 221
+  * relation 5: 211
+  * relation 6: 212
+  * relation 7: 214
+
+### SemEval 2010 Task 8
+
+> all-in-one txt
+
+* Train (1-8000): 8000
+* Test (8001-10717): 2717
+
+### ReRelEM
+
+> all-in-one xml
+
+* Total
+  * Element 'DOC': 129
+
+### BioNLP Shared Task
+
+> each sample three file (rel, a1, txt)
+
+* Total: 798
+
+### DBpediaRelations-PT
+
+> all-in-one txt
+
+`grep -o 'SENTENCE' data/DBpediaRelations-PT/DBpediaRelations-PT-0.2.txt | wc -l`
+
+* Total: 98023
--- data_downloader.sh
+++ data_downloader.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
+extract_dataset_from_targz () {
+    mkdir -p data/$1
+    tar xvzf data/$2 -C data/$1
+}
+
+mkdir data
 # AGENDA dataset
-mkdir -p data/AGENDA
 wget https://github.com/rikdz/GraphWriter/raw/master/data/unprocessed.tar.gz -P data
-tar xvzf data/unprocessed.tar.gz -C data/AGENDA
+extract_dataset_from_targz AGENDA unprocessed.tar.gz
 rm data/unprocessed.tar.gz
+
+# Update dataset from davidsbatista/Annotated-Semantic-Relationships-Datasets
+git submodule update --init --recursive
+
+# SemEval 2007 Task 4
+extract_dataset_from_targz SemEval2007Task4 Annotated-Semantic-Relationships-Datasets/datasets/SemEval2007-Task4.tar.gz
+ls data/SemEval2007Task4/SemEval2007-Task4/*.tar.gz | xargs -n1 tar -C data/SemEval2007Task4 -xvzf 
+# SemEval 2010 Task 8
+extract_dataset_from_targz SemEval2010Task8 Annotated-Semantic-Relationships-Datasets/datasets/SemEval2010_task8_all_data.tar.gz
+# ReRelEM
+extract_dataset_from_targz ReRelEM Annotated-Semantic-Relationships-Datasets/datasets/ReRelEM.tar.gz
+# BioNLP Shared Task
+extract_dataset_from_targz BioNLPSharedTask Annotated-Semantic-Relationships-Datasets/datasets/BioNLP.tar.gz
+
+# DBpediaRelations-PT
+mkdir -p data/DBpediaRelations-PT
+bzip2 -dk data/Annotated-Semantic-Relationships-Datasets/datasets/DBpediaRelations-PT-0.2.txt.bz2
+mv data/Annotated-Semantic-Relationships-Datasets/datasets/DBpediaRelations-PT-0.2.txt data/DBpediaRelations-PT
