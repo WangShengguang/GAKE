d0cfb9e946a63dc4a8433d3912766f9537e75ea0
David Lee <daviddwlee84@gmail.com>
2019-10-30 19:44:24 +0800

    WN sentences and some modification of FB

--- dataset/FreeBase/data_preparation.py
+++ dataset/FreeBase/data_preparation.py
@@ -3,7 +3,7 @@ import os
 
 DATA_PATH = 'data/FB15K-237'
 
-def load_FB15K_237(path='data/datasets_knowledge_embedding/FB15k-237'):
+def prepare_FB15K_237(path='data/datasets_knowledge_embedding/FB15k-237'):
     for txt in ['train', 'test', 'valid']:
         filepath = os.path.join(path, txt + '.txt')
         ori_df = pd.read_csv(filepath, delimiter='\t', names=['head', 'relation', 'tail'])
@@ -12,5 +12,5 @@ def load_FB15K_237(path='data/datasets_knowledge_embedding/FB15k-237'):
         ori_df.to_csv(os.path.join(DATA_PATH, txt + '.tsv'), sep='\t', index=False)
 
 if __name__ == "__main__":
-    os.makedirs(DATA_PATH)
-    load_FB15K_237()
+    os.makedirs(DATA_PATH, exist_ok=True)
+    prepare_FB15K_237()
--- dataset/WordNet/data_preparation.py
+++ dataset/WordNet/data_preparation.py
@@ -1,16 +1,29 @@
 import pandas as pd
 import os
 
+from nltk.corpus import wordnet as wn
+
 DATA_PATH = 'data/WN18RR'
 
-def load_WN18RR(path='data/datasets_knowledge_embedding/WN18RR/text'):
+def generate_sentence_func(df):
+    head_description = wn.synset(df['head']).definition()
+    relation_text = df['relation'].replace('_', ' ').strip()
+    tail_description = wn.synset(df['tail']).definition()
+    sentences = '[CLS] ' + head_description + ' [SEP] ' + relation_text + ' [SEP] ' + tail_description + ' [SEP]'
+    return sentences
+
+
+def prepare_WN18RR(path='data/datasets_knowledge_embedding/WN18RR/text'):
     for txt in ['train', 'test', 'valid']:
         filepath = os.path.join(path, txt + '.txt')
-        ori_df = pd.read_csv(filepath, delimiter='\t', names=['head', 'relation', 'tail'])
-        # TODO generate sentence using description
+        ori_df = pd.read_csv(filepath, delimiter='\t', names=['head', 'relation', 'tail']).head()
+        ori_df['sentence'] = ori_df.apply(generate_sentence_func, axis=1)
+        ori_df['head'] = ori_df['head'].apply(lambda string: string.split('.')[0])
+        # ori_df['relation'] = ori_df['relation'].apply(lambda string: string.replace('_', ' ').strip())
+        ori_df['tail'] = ori_df['tail'].apply(lambda string: string.split('.')[0])
+        ori_df.to_csv(os.path.join(DATA_PATH, txt + '.tsv'), sep=',', index=False)
 
-        ori_df.to_csv(os.path.join(DATA_PATH, txt + '.tsv'), sep='\t', index=False)
 
 if __name__ == "__main__":
-    os.makedirs(DATA_PATH)
-    load_WN18RR()
+    os.makedirs(DATA_PATH, exist_ok=True)
+    prepare_WN18RR()
