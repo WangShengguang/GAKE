bf03ab662367c74a0635a238b1eca1250a6db7df
David Lee <daviddwlee84@gmail.com>
2019-10-30 20:50:53 +0800

    Add FB preparation, verbose mode, WN download

--- dataset/FreeBase/data_preparation.py
+++ dataset/FreeBase/data_preparation.py
@@ -1,16 +1,62 @@
 import pandas as pd
 import os
+from tqdm import tqdm
+
+import json
 
 DATA_PATH = 'data/FB15K-237'
 
-def prepare_FB15K_237(path='data/datasets_knowledge_embedding/FB15k-237'):
-    for txt in ['train', 'test', 'valid']:
-        filepath = os.path.join(path, txt + '.txt')
-        ori_df = pd.read_csv(filepath, delimiter='\t', names=['head', 'relation', 'tail'])
-        # TODO generate sentence using description
 
-        ori_df.to_csv(os.path.join(DATA_PATH, txt + '.tsv'), sep='\t', index=False)
+def load_entity2wikidata(path='data/datasets_knowledge_embedding/FB15k-237/entity2wikidata.json'):
+    with open(path, 'r') as stream:
+        return json.load(stream)
+
+
+entity2wikidata = load_entity2wikidata()
+
+
+def generate_sentence_func(df):
+    try:
+        head_description = entity2wikidata[df['head']]['description']
+        tail_description = entity2wikidata[df['tail']]['description']
+    except KeyError:
+        # key not in entity2wikidata
+        head_description = None
+        tail_description = None
+    relation_text = df['relation'].replace('/', ' ').strip()
+    try:
+        sentences = '[CLS] ' + head_description + ' [SEP] ' + \
+            relation_text + ' [SEP] ' + tail_description + ' [SEP]'
+    except:
+        # head_description or tail_description is None
+        sentences = None
+    return sentences
+
+
+def prepare_FB15K_237(path='data/datasets_knowledge_embedding/FB15k-237', verbose=True):
+
+    for txt in ['train', 'test', 'valid']:
+        if verbose:
+            print('Processing', txt)
+        filepath = os.path.join(path, txt + '.txt')
+        ori_df = pd.read_csv(filepath, delimiter='\t', names=[
+                             'head', 'relation', 'tail'])
+
+        if verbose:
+            tqdm.pandas()
+            ori_df['sentence'] = ori_df.progress_apply(
+                generate_sentence_func, axis=1)
+        else:
+            ori_df['sentence'] = ori_df.apply(generate_sentence_func, axis=1)
+
+        # drop the data which can't construct sentence
+        ori_df = ori_df.dropna()
+
+        ori_df.to_csv(os.path.join(DATA_PATH, txt + '.csv'),
+                      sep=',', index=False)
+
 
 if __name__ == "__main__":
+    print('Processing FB15K-237')
     os.makedirs(DATA_PATH, exist_ok=True)
     prepare_FB15K_237()
--- dataset/WordNet/data_preparation.py
+++ dataset/WordNet/data_preparation.py
@@ -1,29 +1,43 @@
 import pandas as pd
 import os
+from tqdm import tqdm
 
 from nltk.corpus import wordnet as wn
 
 DATA_PATH = 'data/WN18RR'
 
+
 def generate_sentence_func(df):
     head_description = wn.synset(df['head']).definition()
     relation_text = df['relation'].replace('_', ' ').strip()
     tail_description = wn.synset(df['tail']).definition()
-    sentences = '[CLS] ' + head_description + ' [SEP] ' + relation_text + ' [SEP] ' + tail_description + ' [SEP]'
+    sentences = '[CLS] ' + head_description + ' [SEP] ' + \
+        relation_text + ' [SEP] ' + tail_description + ' [SEP]'
     return sentences
 
 
-def prepare_WN18RR(path='data/datasets_knowledge_embedding/WN18RR/text'):
+def prepare_WN18RR(path='data/datasets_knowledge_embedding/WN18RR/text', verbose=True):
     for txt in ['train', 'test', 'valid']:
+        if verbose:
+            print('Processing', txt)
         filepath = os.path.join(path, txt + '.txt')
-        ori_df = pd.read_csv(filepath, delimiter='\t', names=['head', 'relation', 'tail']).head()
-        ori_df['sentence'] = ori_df.apply(generate_sentence_func, axis=1)
-        ori_df['head'] = ori_df['head'].apply(lambda string: string.split('.')[0])
-        # ori_df['relation'] = ori_df['relation'].apply(lambda string: string.replace('_', ' ').strip())
-        ori_df['tail'] = ori_df['tail'].apply(lambda string: string.split('.')[0])
-        ori_df.to_csv(os.path.join(DATA_PATH, txt + '.tsv'), sep=',', index=False)
+        ori_df = pd.read_csv(filepath, delimiter='\t', names=[
+                             'head', 'relation', 'tail'])
+        if verbose:
+            tqdm.pandas()
+            ori_df['sentence'] = ori_df.progress_apply(
+                generate_sentence_func, axis=1)
+        else:
+            ori_df['sentence'] = ori_df.apply(generate_sentence_func, axis=1)
+
+        ori_df.to_csv(os.path.join(DATA_PATH, txt + '.csv'),
+                      sep=',', index=False)
 
 
 if __name__ == "__main__":
+    import nltk
+    print('Check WordNet')
+    print(nltk.download('wordnet'))
+    print('Processing WN18RR')
     os.makedirs(DATA_PATH, exist_ok=True)
     prepare_WN18RR()
